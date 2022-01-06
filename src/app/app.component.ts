6ce6dcf6f342b9a2656bee73e623fe8c53431878
David Amador <54488581+dsamador@users.noreply.github.com>
2022-01-06 14:59:51 -0500

    Ciclo de vida de componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Product } from './models/product.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  imgParent = '';
+  imgParent = 'https://static.platzi.com/media/user_upload/bike-143dcfe9-3190-49fd-88f7-d3bf74182072.jpg';
 
   products: Product[] = [
     {
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,21 +1,40 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
 
   imageDefault = '../../../assets/default.png';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    /* Antes de renderizar */
+    /* importante no correr cosas async, este constructor corre solo una vez */
+    console.log('constructor', 'imgValue =>', this.img)
   }
+  ngOnChanges(): void {
+    //corre antes de renderizar y durante el renderizado
+    //vigila los cambios en los @Inputs, es un escuchador
+    console.log('ngOnchanges', 'imgValue =>', this.img)
+  }
+  ngOnInit(): void {
+    //antes del render, se pueden hacer llamadas async, corre una sola vez
+    console.log('ngOnInit', 'imgValue =>', this.img)
+  }
+  ngAfterViewInit(){
+    //despues de que el html de los hijos de este componente se han renderizado,
+    //normalmente aqui se ponen a los hijos cuando queremos hacer algo de logica
+    console.log('ngOnInit', 'imgValue =>', this.img)
+  }
+  ngOnDestroy(){
+    //Evento que se ejecuta cuando se elimina el componente
+  }
+
 
   imgError(){
     this.img = this.imageDefault;
