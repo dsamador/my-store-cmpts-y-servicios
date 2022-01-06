f4e930e6f781d4744989154c057c608291b0be82
David Amador <54488581+dsamador@users.noreply.github.com>
2022-01-06 13:49:37 -0500

    Abstraer componente producto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  imgParent = 'https://www.w3schools.com/howto/img_avatar.png';
+  imgParent = '';
+
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Casa',
+      price: 565,
+      image: 'https://static.platzi.com/media/user_upload/house-034b0c04-eeff-42fa-b506-79f18f73ff90.jpg'
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: 'https://static.platzi.com/media/user_upload/bike-143dcfe9-3190-49fd-88f7-d3bf74182072.jpg'
+    },
+    {
+      id: '3',
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: 'https://static.platzi.com/media/user_upload/books-80160e05-d177-420b-89c5-01a97b2bdb76.jpg'
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      price: 23,
+      image: 'https://static.platzi.com/media/user_upload/album-6f4213d5-1d2d-4e0f-96fe-edb36c3255b4.jpg'
+    },
+  ]
 
   onLoaded(img: string){
     console.log('Log Padre', img);
