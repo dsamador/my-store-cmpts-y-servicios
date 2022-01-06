ca6bd32812f2042f5a99f9a66b5a23a657113396
David Amador <54488581+dsamador@users.noreply.github.com>
2022-01-06 15:34:40 -0500

    ngDestroy y setInput
    Aprendo ahorro de memoria cuando un componente es destruido en el HTML y vigilar cambios en los @Inputs de manera conjunta y granular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Product } from './models/product.model';
 })
 export class AppComponent {
   imgParent = 'https://static.platzi.com/media/user_upload/bike-143dcfe9-3190-49fd-88f7-d3bf74182072.jpg';
+  showImage = true;
 
   products: Product[] = [
     {
@@ -38,5 +39,9 @@ export class AppComponent {
 
   onLoaded(img: string){
     console.log('Log Padre', img);
+  }
+
+  toggleImage(){
+    this.showImage = !this.showImage;
   }
 }
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -7,24 +7,43 @@ import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewIni
 })
 export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
-  @Input() img: string = '';
+  img: string = '';
+
+  @Input('img') set changeImg(newImg: string){
+    this.img = newImg;
+    //code
+    console.log('change just img =>', this.img)
+  }
+  @Input() alt: string = '';
+
   @Output() loaded = new EventEmitter<string>();
 
   imageDefault = '../../../assets/default.png';
+  counter = 0;
+  counterFn: number | undefined;
+
+
 
   constructor() {
     /* Antes de renderizar */
     /* importante no correr cosas async, este constructor corre solo una vez */
     console.log('constructor', 'imgValue =>', this.img)
   }
-  ngOnChanges(): void {
+  ngOnChanges(changes: SimpleChanges): void {
     //corre antes de renderizar y durante el renderizado
-    //vigila los cambios en los @Inputs, es un escuchador
-    console.log('ngOnchanges', 'imgValue =>', this.img)
+    //vigila los cambios en todos los @Inputs, es un escuchador
+    //si queremos saber granularmente los cambios en los inputs toca hacer
+    //uso de un set
+    console.log('ngOnchanges', 'imgValue =>', this.img);
+    console.log('changes', changes)
   }
   ngOnInit(): void {
     //antes del render, se pueden hacer llamadas async, corre una sola vez
     console.log('ngOnInit', 'imgValue =>', this.img)
+    this.counterFn = window.setInterval(()=> {
+      this.counter += 1;
+      console.log('run counter')
+    }, 1000);
   }
   ngAfterViewInit(){
     //despues de que el html de los hijos de este componente se han renderizado,
@@ -32,7 +51,9 @@ export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy
     console.log('ngOnInit', 'imgValue =>', this.img)
   }
   ngOnDestroy(){
-    //Evento que se ejecuta cuando se elimina el componente
+    //Evento que se ejecuta cuando se elimina el componente, es util para eliminar
+    //procesos que no se destruyen al destruir el componente en el html
+    window.clearInterval(this.counterFn);
   }
 
 
