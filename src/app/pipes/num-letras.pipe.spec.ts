bfe34f2a5e30dd6f4083de94222e3a40d671127e
David Amador <54488581+dsamador@users.noreply.github.com>
2022-01-09 19:24:21 -0500

    pipes personalizados

---
+++ src/app/pipes/num-letras.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { NumLetrasPipe } from './num-letras.pipe';
+
+describe('NumLetrasPipe', () => {
+  it('create an instance', () => {
+    const pipe = new NumLetrasPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
